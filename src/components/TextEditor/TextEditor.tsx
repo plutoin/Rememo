083a204c708862db60e621d74d0cc947b413007d
plutoin <uleage@gmail.com>
2023-08-24 11:01:35 +0900

    Feat: TextEditor 구현

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -1,5 +1,56 @@
-import React from "react";
+import { Dispatch } from "react";
+import ReactQuill from "react-quill";
 
-export default function TextEditor() {
-  return <div>TextEditor</div>;
+import { Container } from "./TextEditor.styles";
+import "react-quill/dist/quill.snow.css";
+
+interface TextEditorProps {
+  value: string;
+  setValue: Dispatch<React.SetStateAction<string>>;
+  color: string;
+}
+
+const formats = [
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "list",
+
+  "color",
+  "background",
+
+  "image",
+  "blockquote",
+  "code-block",
+];
+
+const modules = {
+  toolbar: [
+    [{ list: "ordered" }, { list: "bullet" }],
+    [],
+    ["italic", "underline", "strike"],
+    [],
+    [{ color: [] }, { background: [] }],
+    [],
+    ["image", "blockquote", "code-block"],
+  ],
+};
+
+export default function TextEditor({
+  color,
+  value,
+  setValue,
+}: TextEditorProps) {
+  return (
+    <Container noteColor={color}>
+      <ReactQuill
+        formats={formats}
+        modules={modules}
+        theme="snow"
+        value={value}
+        onChange={setValue}
+      />
+    </Container>
+  );
 }
